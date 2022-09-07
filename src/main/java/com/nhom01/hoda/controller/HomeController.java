0bd375c11e6c7ae570f23bfcb7741b3c87700776
hoangtu291 <93514926+hoangtu291@users.noreply.github.com>
2022-09-08 02:32:06 +0700

    Update HomeController.java

--- src/main/java/com/nhom01/hoda/controller/HomeController.java
+++ src/main/java/com/nhom01/hoda/controller/HomeController.java
@@ -16,6 +16,7 @@ public class HomeController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
         RequestDispatcher rd = request.getRequestDispatcher("/views/home.jsp");
         rd.forward(request, response);
     }
